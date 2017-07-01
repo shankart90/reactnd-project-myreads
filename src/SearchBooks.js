03aeb9868c849eacf9b3c8a17badf57ce186b851
Shankar Tiwari <shankartiwari@Shankars-MacBook-Pro.local>
2017-07-01 16:11:13 -0700

    implementing search

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -17,11 +17,11 @@ class SearchBooks extends Component {
   updateQuery = (query) => {
     if(query){
       BooksAPI.search(query, 20).then((results) =>{
-          console.log(results.error)
+          //console.log(results.error)
           if(results){
             this.setState({ query })
-            console.log(results)
-            // this.setState({ showingBooks : results[1] })
+             //console.log(results)
+            this.setState({ showingBooks : results[1] })
           }
       })
     }
@@ -47,7 +47,26 @@ class SearchBooks extends Component {
           </div>
           <div className="search-books-results">
             <ol className="books-grid">
-
+                {showingBooks.map((book) => (
+                  <li key={book.id}>
+                    <div className="book">
+                      <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}/>
+                          <div className="book-shelf-changer">
+                            <select defaultValue={book.shelf} onChange={(event) => onUpdateShelf(book, event)}>
+                              <option value="none" disabled>Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead">Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors}</div>
+                      </div>
+                  </li>
+                ))}
             </ol>
           </div>
         </div>
